062c552869b649762b327b3036dd35736d609817
David Katz <hellodavidkatz@pm.me>
2024-12-27 07:57:09 -0500

    merge sorted refresher

--- pointers/sort.py
+++ pointers/sort.py
@@ -2,38 +2,30 @@ import math
 from typing import List
 
 
-def _merge_sort(arr: List[int]) -> List[int]:
-    if len(arr) <= 1:
-        return arr
-
-    mid = len(arr) // 2
-    left = arr[:mid]
-    right = arr[mid:]
-
-    left = merge_sort(left)
-    right = merge_sort(right)
-
-    pl = 0
-    pr = 0
-    sorted_list = []
-
-    while pl < len(left) and pr < len(right):
-        if left[pl] <= right[pr]:
-            sorted_list.append(left[pl])
-            pl += 1
-        else:
-            sorted_list.append(right[pr])
-            pr += 1
-
-    sorted_list.extend(left[pl:])
-    sorted_list.extend(right[pr:])
-
-    return sorted_list
-
-
 def merge_sort(arr: List[int]) -> List[int]:
     """
-    if lenght of array is 1, return the array (base case)
+    if we have one element, return it
+    otherwise:
+        calculate mid point
+        take left and right and merge sort them
+
+    initialize pointers for left and right
+    initialize sorted_result
+
+    while left pointer < len left and right pointer < len right:
+        compare left and right
+        if left <= right:
+            append left
+            incrememnt left
+        else
+            append right
+            incremement right
+
+    extend and cleanup
+    """
+
+    if len(arr) <= 1:
+        return arr
 
     midpoint = len(arr) // 2
     left = arr[:midpoint]
@@ -42,31 +34,8 @@ def merge_sort(arr: List[int]) -> List[int]:
     sorted_left = merge_sort(left)
     sorted_right = merge_sort(right)
 
-    merged_sorted = []
-
-    track pointers left and right
-    while the left pointer less thand left length and right pointer less than right length:
-        compare left at lp and right at rp
-        if left at lp <= right at rp:
-            merge_sorted.append(left at lp)
-            lp ++
-        else:
-            merge _sroted.append(right at rp)
-            rp ++
-
-    cleanup by extending merge_sorted with stragglers from left and right
-
-    """
-
-    if len(arr) <= 1:
-        return arr
-    mid = len(arr) // 2
-
-    sorted_left = merge_sort(arr[:mid])
-    sorted_right = merge_sort(arr[mid:])
-
-    merge_sorted = []
     pl = pr = 0
+    merge_sorted = []
 
     while pl < len(sorted_left) and pr < len(sorted_right):
         if sorted_left[pl] <= sorted_right[pr]:
@@ -89,12 +58,12 @@ def main() -> None:
 
     if result == expected:
         print("yay")
-        print(f"array: {result}")
-        print(f"sorted: {expected}")
+        print(f"array: {array}")
+        print(f"result: {result}")
     else:
         print("Gahhh darn")
-        print(f"array: {array}")
-        print(f"sorted: {expected}")
+        print(f"expected: {expected}")
+        print(f"result: {result}")
     return
 
 
